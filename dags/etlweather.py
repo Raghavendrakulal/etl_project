f2f5c988ef37e09769e6fb94b6a1b21ba1de0371
raghavendra <kulalraghavendra4@gmail.com>
2026-06-29 01:50:07 +0530

    feature1 merge

--- dags/etlweather.py
+++ dags/etlweather.py
@@ -7,7 +7,7 @@ from airflow.decorators import task
 
 
 LATITUDE = '40.7128'
-LONGITUDE = '-0.1278'
+LONGITUDE = '-0.1278feature1'
 POSTGRES_CONN_ID = 'postgres_default'
 API_CONN_ID = 'open_meteo_api'
 default_args = {
